ecc9480d8b0ae3e7c08cb32877b252e5a758e8c7
Рамазан Айсаев <aisaev-68@yandex.ru>
2021-11-03 15:44:48 +0300

    Создание бота, который реагирует на команду /hello-world, а также на текст «Привет»

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+import telebot
+from telebot import types
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+TOKEN = os.environ.get('TELEGRAM_API_TOKEN')
+
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
+
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/hello-world":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши Привет.")
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
