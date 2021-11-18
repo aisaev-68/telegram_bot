0a7d61acdd2bd7ee22d2ad26bf1cab90ab607159
ramazan_aisaev <aisaev-98@yandex.ru>
2021-11-18 15:47:51 +0300

    18.10.21

--- botrequests/several_calendars.py
+++ botrequests/several_calendars.py
@@ -1,19 +1,4 @@
-import datetime
-import os
-import telebot
-from dotenv import load_dotenv
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardRemove
-from telegram_bot_calendar import WMonthTelegramCalendar, LSTEP, DAY
-
-
-
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-if os.path.exists(dotenv_path):
-    load_dotenv(dotenv_path)
-
-TOKEN = os.environ.get('TELEGRAM_API_TOKEN')
-
-bot = telebot.TeleBot(TOKEN, parse_mode=None)
+from telegram_bot_calendar import WMonthTelegramCalendar, DAY
 
 data = {}
 class MyStyleCalendar(WMonthTelegramCalendar):
@@ -21,27 +6,24 @@ class MyStyleCalendar(WMonthTelegramCalendar):
     next_button = "➡️"
     first_step = DAY
 
+
 def gen_markup():
     calendar, step = MyStyleCalendar(calendar_id=1).build()
     return calendar
 
-@bot.message_handler(commands=['start'])
-def start(m):
-    bot.send_message(m.chat.id,
-                     "Выберите дату въезда",
-                     reply_markup=gen_markup())
-
-
 
 def start1(m):
-
     calendar, step = MyStyleCalendar(calendar_id=1).build()
     bot.send_message(m.chat.id,
                      "Выберите дату выезда",
                      reply_markup=calendar)
 
 
-
+@bot.message_handler(commands=['start'])
+def start(m):
+    bot.send_message(m.chat.id,
+                     "Выберите дату въезда",
+                     reply_markup=gen_markup())
 
 
 @bot.callback_query_handler(func=MyStyleCalendar.func(calendar_id=1))
@@ -59,10 +41,3 @@ def cal1(c):
         else:
             data['out_date'] = result.isoformat()
             print(data)
-
-
-
-
-
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
